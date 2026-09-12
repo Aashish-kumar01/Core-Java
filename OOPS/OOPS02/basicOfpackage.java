5b40063e9b4178783785d82ffd2421e8cc3108d5
Aashish-kumar01 <kumarashish69959@gmail.com>
2026-09-13 02:00:44 +0530

    Updated File

---
+++ OOPS/OOPS02/basicOfpackage.java
@@ -0,0 +1,11 @@
+package OOPS02;
+
+// import static OOPS02.message.message;
+
+public class basicOfpackage {
+    public static void main(String[] args) {
+        System.out.println(" I am awesome");
+
+        message.message();
+    }
+}
---
+++ OOPS/OOPS02/message.java
@@ -0,0 +1,11 @@
+package OOPS02;
+
+public class message {
+    public static void main(String[] args) {
+        // message();
+    }
+
+    public static void message(){
+        System.out.println("This course is awesome");
+    }
+}
