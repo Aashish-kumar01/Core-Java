15b6a9039242839f17ebab325de774cae68a50ff
Aashish-kumar01 <kumarashish69959@gmail.com>
2026-09-13 01:45:04 +0530

    Updated File

---
+++ InnerClass/Annonymus&Abstract/Demo.java
@@ -0,0 +1,19 @@
+abstract class A{
+    public abstract void show();
+    public abstract void config();
+}
+
+public class Demo {
+    public static void main(String[] args) {
+        A obj = new A(){
+            public void show(){
+                System.out.println("In new show");
+            }
+            public void config(){
+                System.out.println("In new config");
+            }
+        };
+        obj.show();
+        obj.config();
+    }
+}
